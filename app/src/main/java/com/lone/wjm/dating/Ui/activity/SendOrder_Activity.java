5afeb3c6765786f5b0f09ebeb554d3a0d3ec5433
lone <4951048@qq.com>
2016-06-02 19:33:42 +0800

    第二次提交，创建约单

--- app/src/main/java/com/lone/wjm/dating/Ui/activity/SendOrder_Activity.java
+++ app/src/main/java/com/lone/wjm/dating/Ui/activity/SendOrder_Activity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
